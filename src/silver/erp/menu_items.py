b7230c37320cdc79731c234e4cb3829f2a21c2b7
Nishant Singh <singhnishant414243@gmail.com>
2026-05-30 16:47:56 +0530

    add added_date column

--- src/silver/erp/menu_items.py
+++ src/silver/erp/menu_items.py
@@ -12,22 +12,24 @@ class Menu_itemsSilver(BaseSilverPipeline):
 
     def clean(self, df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
 
-        item_id =      clean_id(df['item_id'], 'I', 4)
+        item_id =         clean_id(df['item_id'], 'I', 4)
 
-        item_name =    clean_text(df['item_name']).str.title()
+        item_name =       clean_text(df['item_name']).str.title()
 
-        category =     clean_text(df['category']).str.title()
-        category =     category.where(category.isin(['Main', 'Beverage', 'Dessert', 'Starter']))
+        category =        clean_text(df['category']).str.title()
+        category =        category.where(category.isin(['Main', 'Beverage', 'Dessert', 'Starter']))
 
-        cuisine  =     clean_text(df['cuisine']).str.title()
-        cuisine =      cuisine.where(cuisine.isin(['Chinese', 'Indian', 'Italian']))
+        cuisine  =        clean_text(df['cuisine']).str.title()
+        cuisine =         cuisine.where(cuisine.isin(['Chinese', 'Indian', 'Italian']))
 
-        is_veg =       pd.to_numeric(df['is_veg'], errors='coerce')
-        is_veg =       is_veg.where(is_veg.isin([0,1]))
+        is_veg =          pd.to_numeric(df['is_veg'], errors='coerce')
+        is_veg =          is_veg.where(is_veg.isin([0,1]))
 
-        sell_price =   pd.to_numeric(df['selling_price'], errors='coerce')
-        sell_price =   sell_price.where((sell_price >= 50) & (sell_price <= 500))
+        selling_price =   pd.to_numeric(df['selling_price'], errors='coerce')
+        selling_price =   selling_price.where((selling_price >= 50) & (selling_price <= 500))
 
+        added_date =      pd.to_datetime(df['added_date'], format='%Y-%m-%d', errors='coerce')
+        added_date =      added_date.where((added_date >= "2021-01-01") & (added_date <= "2025-12-31")) #type:ignore
 
         df1 = pd.DataFrame({
             'item_id':item_id,
@@ -35,16 +37,15 @@ class Menu_itemsSilver(BaseSilverPipeline):
             'category':category,
             'cuisine':cuisine,
             'is_veg':is_veg,
-            'selling_price':sell_price
+            'selling_price':selling_price,
+            'added_date':added_date
         })
 
-        ord_itm = pd.read_csv(path['order_items_path'])
-        group = ord_itm[['item_id', 'unit_price']].groupby('item_id').median().reset_index()
-        merge = df.merge(group, on = 'item_id', how='left')
-
-        merge.loc[merge['selling_price'].isna(), 'selling_price'] = merge['unit_price']
-        merge.drop('unit_price', axis=1, inplace=True)
-
+        # FILLING NaN selling_price
+        order_items =            pd.read_csv(path['order_items'])
+        selling_price =          order_items.groupby('item_id')['unit_price'].median()
+        merge =                  df1.merge(selling_price, how='left', on='item_id')
+        df1['selling_price'] =   merge['selling_price'].fillna(merge['unit_price'])
 
         df2 = df1.dropna(subset='item_id')
         df3 = df2.drop_duplicates('item_id').sort_values('item_id').reset_index(drop=True)
