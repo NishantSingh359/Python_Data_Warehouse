091db645346e08fa4535172dae78e92fe5c92599
Nishant Singh <singhnishant414243@gmail.com>
2026-05-30 16:52:15 +0530

    update table

--- src/silver/erp/restaurants.py
+++ src/silver/erp/restaurants.py
@@ -7,21 +7,20 @@ class RestaurantsSilver(BaseSilverPipeline):
 
     def clean(self, df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
 
-        restaurant_id =   clean_id(df['restaurant_id'], 'R', 3)
+        restaurant_id =       clean_id(df['restaurant_id'], 'R', 3)
 
-        restaurant_name = clean_text(df['restaurant_name']).str.title()
+        restaurant_name =     clean_text(df['restaurant_name']).str.title()
 
-        city =            clean_text(df['city']).str.title()
-        city =            city.where(city.isin(['Mumbai', 'Bangalore', 'Hyderabad', 'Delhi', 'Pune']), np.nan)
+        city =                clean_text(df['city']).str.title()
+        city =                city.where(city.isin(['Mumbai', 'Bangalore', 'Hyderabad', 'Delhi', 'Pune']), np.nan)
 
-        restaurant_type = df['restaurant_type'].str.strip().str.title().replace({'':np.nan})
+        restaurant_type =     df['restaurant_type'].str.strip().str.title().replace({'':np.nan})
 
-        open_date =       pd.to_datetime(df['open_date'], format= '%Y-%m-%d %H:%M:%S', errors= 'coerce')
-        open_date =       (open_date).where((open_date >= '2021-01-01') & (open_date <= '2025-12-31')) #type:ignore
+        open_date =           pd.to_datetime(df['open_date'], format= '%Y-%m-%d %H:%M:%S', errors= 'coerce')
+        open_date =           (open_date).where((open_date >= '2021-01-01') & (open_date <= '2025-12-31')) #type:ignore
 
-        closed_date =     pd.to_datetime(df['closed_date'], format= '%Y-%m-%d', errors= 'coerce')
-        closed_date =     closed_date.where(closed_date>open_date)
-
+        closed_date =         pd.to_datetime(df['closed_date'], format= '%Y-%m-%d', errors= 'coerce')
+        closed_date =         closed_date.where(closed_date>open_date)
 
         df1 = pd.DataFrame({
             'restaurant_id':restaurant_id,
